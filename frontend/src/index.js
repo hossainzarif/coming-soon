2cf0dba9d7bcee688b12a09d7f9313a09d6fcd06
hossainzarif <zarifh493@gmail.com>
2021-08-30 18:19:21 +0600

    added countdown

---
+++ frontend/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Preloader from './components/Preloader/Preloader'
+import Timer from './components/Countdown/Timer'
+
+import './styles.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>
+          Website
+          <br />
+          Coming Soon
+        </h1>
+        <Timer />
+        <Preloader />
+      </div>
+    </div>
+  )
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
