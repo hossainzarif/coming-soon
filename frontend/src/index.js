596152ef2c59b7dd1d17ebace94c98f11140e567
hossainzarif <zarifh493@gmail.com>
2021-08-31 01:27:43 +0600

    added bottom message

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Preloader from './components/Preloader/Preloader'
 import Timer from './components/Countdown/Timer'
-
+import './bootstrap.min.css'
 import './styles.css'
 
 function App() {
@@ -15,7 +15,19 @@ function App() {
           Coming Soon
         </h1>
         <Timer />
-        <Preloader />
+        {/* <Preloader /> */}
+        <h4>
+          Hello! Thank you for visiting us and supporting our small business.{' '}
+          <br />
+          In the meantime, please visit our{' '}
+          <a
+            style={{ color: 'white' }}
+            href='https://www.etsy.com/shop/themushroomhunter'
+          >
+            ETSY STORE
+          </a>
+          {'.'}
+        </h4>
       </div>
     </div>
   )
